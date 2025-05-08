0de618b35206dd45549b164db5721bfc985617d3
Gautamtinker <gautamtinker83@gmail.com>
2025-05-08 11:29:08 +0530

    cd

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://gautamtinker83:NzR24VLXkueZyf7n@cluster0.8rqeeuu.mongodb.net/",
+      "mongodb+srv://gautamtinker83:NzR24VLXkueZyf7n@cluster0.8rqeeuu.mongodb.net?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
